7df4358fedabfc13f80abd792891c8d1cd47e79f
Mark Chesney <markplotlib@gmail.com>
2019-09-10 14:15:19 -0700

    stash code before fixing master

--- WordCounter.java
+++ WordCounter.java
@@ -31,13 +31,28 @@ public class WordCounter {
     }
     public int incrementWordCount(String word) {
         int hashCode = word.hashCode();
+
+// if
         hashTable[hashCode] = new Bucket(word);
+// else
         hashTable[hashCode].count++;
         return hashTable[hashCode].count;
         // FIXME - return the updated word count for this word
     }
     public int getWordCount(String word) {
         int hashCode = word.hashCode();
+        if (hashTable.contains(key)) {
+            // When the word already exists in the hash table, increment the word count.
+
+        } else {
+            // Adds a word to the hash table if it was not previously seen (initial count of 1)
+
+        }
+
+        	// Returns word count (will always be greater than or equal to 1)
+
+        hashTable[hashCode];
+
         return 0; // FIXME - return the current word count for this word
     }
     public void removeWord(String word) {
@@ -53,8 +68,7 @@ public class WordCounter {
         public Bucket(String word) {
             this.word = word;
             this.count = 1;
-            // if (next != null)
-            //     this.next = new Bucket(word, count, next);
+            this.next = null;
         }
     }
 }
